2599d34740a999d276f3ca9dcd9d0c58e51c383d
sokol <sokol@ASUS>
2024-07-16 19:47:51 +0500

    Добавьте файлы проекта.

---
+++ 1.5_03/1.5_03.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    // Инициализация двумерного массива размерностью 3x6
+    int array[3][6] = 
+    {
+        {1,  2,  3,  4,  5,  6},
+        {7,  8,  9,  10, 11, 12},
+        {13, 14, 15, 16, 17, 18}
+    };
+    int sizeRow = sizeof(array) / sizeof(array[0]);
+    int sizeColums = sizeof(array) / sizeof(array[0][0]);
+
+    for(int i = 0; i < sizeRow; i++)
+    {
+        std::cout << i << ": " << array[i] << std::endl; 
+    }
+
+    return 0;
+}
